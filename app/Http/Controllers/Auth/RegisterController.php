8c92635dc6031980c497efbf99d8a63a558454d4
mroot <almatov.ms@gmail.com>
2021-01-10 18:34:59 +0500

    after registering new user redirect to DashboardController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -21,6 +23,15 @@ class RegisterController extends Controller
             'password' => 'required|confirmed'
         ]);
 
-        dd('store');
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        auth()->attempt($request->only('email', 'password'));
+
+        return redirect()->route('dashboard');
     }
 }
