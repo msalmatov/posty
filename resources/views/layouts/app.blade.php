82a8c836a7c193c5190609c2a378ae3ba5ac2cb1
mroot <almatov.ms@gmail.com>
2021-01-10 19:37:02 +0500

    logout logic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 <body class="bg-gray-200">
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
-            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="{{ route('home') }}" class="p-3">Home</a></li>
             <li><a href="{{ route('dashboard') }}" class="p-3">Dashboard</a></li>
             <li><a href="" class="p-3">Post</a></li>
         </ul>
@@ -18,7 +18,12 @@
         <ul class="flex items-center">
             @auth
                 <li><a href="" class="p-3">Murod Almatov</a></li>
-                <li><a href="" class="p-3">Logout</a></li>
+                <li>
+                    <form action="{{ route('logout') }}" method="post" class="inline p-3">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
             @endauth
             @guest
                 <li><a href="{{ route('login') }}" class="p-3">Login</a></li>
