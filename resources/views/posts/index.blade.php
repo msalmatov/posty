fd90ab5730904d590fba26f855bfd81d1d7a811a
mroot <almatov.ms@gmail.com>
2021-01-11 20:08:18 +0500

    delete Post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,25 +30,35 @@
                     <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
 
                     <p class="mb-2">{{ $post->body }}</p>
-                </div>
 
-                <div class="flex items-center mb-4">
-                    @auth
-                        @if (!$post->likedBy(auth()->user()))
-                            <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
-                                @csrf
-                                <button type="submit" class="text-blue-500">Like</button>
-                            </form>
-                        @else
-                            <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                    @if ($post->ownedBy(auth()->user()))
+                        <div>
+                            <form action="{{ route('posts.destroy', $post) }}" method="post">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="text-blue-500">Unlike</button>
+                                <button type="submit" class="text-blue-500">Delete</button>
                             </form>
-                        @endif
-                    @endauth
+                        </div>
+                    @endif
 
-                    <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+                    <div class="flex items-center mb-4">
+                        @auth
+                            @if (!$post->likedBy(auth()->user()))
+                                <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Like</button>
+                                </form>
+                            @else
+                                <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+                            @endif
+                        @endauth
+
+                        <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+                    </div>
                 </div>
             @endforeach
 
