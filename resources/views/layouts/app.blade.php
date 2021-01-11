08f200a478ef7c104ac840fc44f12e123dc117ce
mroot <almatov.ms@gmail.com>
2021-01-11 10:51:47 +0500

    show correct user name on nav bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
         <ul class="flex items-center">
             @auth
-                <li><a href="" class="p-3">Murod Almatov</a></li>
+                <li><a href="" class="p-3">{{ auth()->user()->name }}</a></li>
                 <li>
                     <form action="{{ route('logout') }}" method="post" class="inline p-3">
                         @csrf
