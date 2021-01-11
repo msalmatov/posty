2708fafe23811a038bd761c1e8062c9000a63734
mroot <almatov.ms@gmail.com>
2021-01-11 17:20:07 +0500

    like/unlike post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -31,6 +31,23 @@
 
                     <p class="mb-2">{{ $post->body }}</p>
                 </div>
+
+                <div class="flex items-center">
+                    @if (!$post->likedBy(auth()->user()))
+                        <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                            @csrf
+                            <button type="submit" class="text-blue-500">Like</button>
+                        </form>
+                    @else
+                        <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="text-blue-500">Unlike</button>
+                        </form>
+                    @endif
+
+                    <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+                </div>
             @endforeach
 
             {{ $posts->links() }}
